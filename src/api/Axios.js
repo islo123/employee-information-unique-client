825794484f9e27e1fa42880a1fda7de2f3fad718
islo123 <43340489+islo123@users.noreply.github.com>
2022-10-23 14:34:02 +0300

    deployeing

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
   export const api = axios.create({
-    //baseURL: "https://appin-nimi.herokuapp.com/employee"
-    baseURL: "http://localhost:3001/employee"
+    baseURL: "https://employee-information-1234.herokuapp.com/employee"
+    //baseURL: "http://localhost:3001/employee"
   })
