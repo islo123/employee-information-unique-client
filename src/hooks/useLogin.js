358187e7411e9152733a5c5390440c246915f1c7
islo123 <43340489+islo123@users.noreply.github.com>
2022-11-03 13:42:47 +0200

    cors-off-react

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -8,7 +8,7 @@ export const useLogin = () => {
 
     const login = async (email, password) => {
             try {
-                const res = await api.post('/login',{email, password}, {headers: { "Access-Control-Allow-Origin": "*",'Content-Type': 'application/json'}, withCredentials: true});
+                const res = await api.post('/login',{email, password});
                 localStorage.setItem('user', JSON.stringify(res.data))
                 dispatch({type: 'LOGIN', payload: res.data})
             } catch(error) {
